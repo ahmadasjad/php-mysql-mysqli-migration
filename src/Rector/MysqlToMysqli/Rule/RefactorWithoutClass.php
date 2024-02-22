29fdd9648438026c0b2f0cc08f0a9b99c0dbe783
Ahmad Asjad <ahmadcimage@gamil.com>
2024-02-23 00:18:51 +0530

    cong arg position mam

--- src/Rector/MysqlToMysqli/Rule/RefactorWithoutClass.php
+++ src/Rector/MysqlToMysqli/Rule/RefactorWithoutClass.php
@@ -10,6 +10,11 @@ use PhpParser\Node\Name;
 
 class RefactorWithoutClass
 {
+    /**
+     * in mysql_* resource $link_identifier is optional, but in mysqli_* mysqli $mysqli is required.
+     * @var array
+     * Todo: verify the position of the params
+     */
     private $optionalConParamRequiredConParamMap = [
         // 'old_func_name'=> position by zero index,
         'mysql_client_encoding' => 0,
@@ -40,8 +45,50 @@ class RefactorWithoutClass
         'mysql_unbuffered_query' => 1,
     ];
 
-    private $swithArgPositionMap = [
+    //Todo: verify the position of the params
+    private $switchConArgPositionMap = [
         // 'old_func_name'=> [old position by zero index, new position by zero index],
+        'mysql_client_encoding' => [0, 0], // Position remains the same
+        'mysql_close' => [0, 0], // Position remains the same
+//        'mysql_db_name' => [0, null], // No equivalent in mysqli_*
+//        'mysql_db_query' => [0, null], // No equivalent in mysqli_*
+//        'mysql_drop_db' => [1, null], // No equivalent in mysqli_*
+//        'mysql_errno' => [0, 0], // Position remains the same
+//        'mysql_error' => [0, 0], // Position remains the same
+//        Above is verified
+        'mysql_escape_string' => [0, 0], // Position remains the same
+        'mysql_fetch_array' => [0, 0], // Position remains the same
+        'mysql_fetch_assoc' => [0, 0], // Position remains the same
+        'mysql_fetch_field' => [0, null], // No equivalent in mysqli_*
+        'mysql_fetch_lengths' => [0, 0], // Position remains the same
+        'mysql_fetch_object' => [0, 0], // Position remains the same
+        'mysql_fetch_row' => [0, 0], // Position remains the same
+        'mysql_field_seek' => [0, null], // No equivalent in mysqli_*
+        'mysql_field_table' => [0, null], // No equivalent in mysqli_*
+        'mysql_field_type' => [0, null], // No equivalent in mysqli_*
+        'mysql_free_result' => [0, 0], // Position remains the same
+        'mysql_get_client_info' => [0, 0], // Position remains the same
+        'mysql_get_host_info' => [0, 0], // Position remains the same
+        'mysql_get_proto_info' => [0, 0], // Position remains the same
+        'mysql_get_server_info' => [0, 0], // Position remains the same
+        'mysql_info' => [0, 0], // Position remains the same
+        'mysql_insert_id' => [0, 0], // Position remains the same
+        'mysql_list_dbs' => [0, 0], // Position remains the same
+        'mysql_list_fields' => [2, 1], // Position changed from 2 to 1 in mysqli_*
+        'mysql_list_processes' => [0, 0], // Position remains the same
+        'mysql_list_tables' => [0, 0], // Position remains the same
+        'mysql_num_fields' => [0, 0], // Position remains the same
+        'mysql_num_rows' => [0, 0], // Position remains the same
+        'mysql_pconnect' => [0, 0], // Position remains the same
+        'mysql_ping' => [0, 0], // Position remains the same
+        'mysql_query' => [1, 0], // Position changed from 1 to 0 in mysqli_*
+        'mysql_real_escape_string' => [1, 0], // Position changed from 1 to 0 in mysqli_*
+        'mysql_select_db' => [1, 0], // Position changed from 1 to 0 in mysqli_*
+        'mysql_set_charset' => [0, 0], // Position remains the same
+        'mysql_stat' => [0, 0], // Position remains the same
+        'mysql_tablename' => [1, 0], // Position changed from 1 to 0 in mysqli_*
+        'mysql_thread_id' => [0, 0], // Position remains the same
+        'mysql_unbuffered_query' => [1, 0], // Position changed from 1 to 0 in mysqli_*
     ];
 
     public function changeParams(Node\Expr\FuncCall $node, $functionCallName)
@@ -61,10 +108,10 @@ class RefactorWithoutClass
 
     private function changeParamPosition(Node\Expr\FuncCall $node, $functionCallName)
     {
-        if (array_key_exists($functionCallName, $this->swithArgPositionMap))        {
+        if (array_key_exists($functionCallName, $this->switchConArgPositionMap))        {
             $passedArgs = $node->getArgs();
-            $oldPos = $this->swithArgPositionMap[$functionCallName][0];
-            $newPos = $this->swithArgPositionMap[$functionCallName][1];
+            $oldPos = $this->switchConArgPositionMap[$functionCallName][0];
+            $newPos = $this->switchConArgPositionMap[$functionCallName][1];
 
             // Remove the value from the old position
             $valueToMove = array_splice($passedArgs, $oldPos, 1)[0];
