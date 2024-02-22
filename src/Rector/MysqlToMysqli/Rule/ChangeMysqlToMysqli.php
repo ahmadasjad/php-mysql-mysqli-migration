8e102014381e4dfa43c8283ab826c6972481692d
Ahmad Asjad <ahmadcimage@gamil.com>
2024-02-22 23:03:59 +0530

    change argument

--- src/Rector/MysqlToMysqli/Rule/ChangeMysqlToMysqli.php
+++ src/Rector/MysqlToMysqli/Rule/ChangeMysqlToMysqli.php
@@ -15,6 +15,9 @@ use AhmadAsjad\Refactor\MysqlToMysqli\Functions\MysqlNumRows;
 use AhmadAsjad\Refactor\MysqlToMysqli\Functions\MysqlQuery;
 use AhmadAsjad\Refactor\MysqlToMysqli\Functions\MysqlRealEscapeString;
 use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\ConstFetch;
+use PhpParser\Node\Name;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -84,6 +87,14 @@ class ChangeMysqlToMysqli extends AbstractRector
         'mysql_tablename' => null, // Not directly equivalent, consider using SQL queries directly.
         'mysql_thread_id' => 'mysqli_thread_id',
         'mysql_unbuffered_query' => 'mysqli_query with MYSQLI_USE_RESULT option',
+    ];
+
+    private $optionalConParamRequiredConParamMap = [
+        // 'old_func_name'=> position by zero index,
+    ];
+
+    private $swithArgPositionMap = [
+        // 'old_func_name'=> [old position by zero index, new position by zero index],
     ];
 
     public function getRuleDefinition(): RuleDefinition
@@ -176,7 +187,40 @@ class ChangeMysqlToMysqli extends AbstractRector
         }
 
         // Change params
-        // TODO: Change params
+        $node = $this->changeParams($node, $functionCallName);
+
+        return $node;
+    }
+
+    private function changeParams(Node\Expr\FuncCall $node, $functionCallName)
+    {
+        if (array_key_exists($functionCallName, $this->optionalConParamRequiredConParamMap))
+        {
+            $passedArgs = $node->getArgs();
+            if (!isset($passedArgs[$this->optionalConParamRequiredConParamMap[$functionCallName]])) {
+                $contArg = new Arg(new ConstFetch(new Name(self::getDbConnectionVariable())));
+                $passedArgs[$this->optionalConParamRequiredConParamMap[$functionCallName]] = $contArg;
+                $node->args = $passedArgs;
+            }
+        }
+
+        return $this->changeParamPosition($node, $functionCallName);
+    }
+
+    private function changeParamPosition(Node\Expr\FuncCall $node, $functionCallName)
+    {
+        if (array_key_exists($functionCallName, $this->swithArgPositionMap))        {
+            $passedArgs = $node->getArgs();
+            $oldPos = $this->swithArgPositionMap[$functionCallName][0];
+            $newPos = $this->swithArgPositionMap[$functionCallName][1];
+
+            // Remove the value from the old position
+            $valueToMove = array_splice($passedArgs, $oldPos, 1)[0];
+
+            // Insert the value at the new position
+            array_splice($passedArgs, $newPos, 0, $valueToMove);
+            $node->args = $passedArgs;
+        }
 
         return $node;
     }
